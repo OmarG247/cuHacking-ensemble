06707a8f4e9872ae524a8b43cc1a0a8a5ce325d4
Omar Flores <omar.garciaf247@gmail.com>
2019-02-17 13:30:30 -0500

    UI update

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -3,8 +3,12 @@ $(document).ready(() => {
         console.log("Result playlist");
         let temp = document.getElementById('allTracks').innerHTML;
 
-        for (let i = 0; i < data[0].length; i++) {
-            temp += `<iframe src="https://open.spotify.com/embed/track/${data[0][i].track.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+        let size = data.length - 1;
+
+        console.log(size);
+
+        for (let i = 0; i < data[size].length; i++) {
+            temp += `<iframe src="https://open.spotify.com/embed/track/${data[size][i].track.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
         }
 
         document.getElementById('allTracks').innerHTML = temp;
