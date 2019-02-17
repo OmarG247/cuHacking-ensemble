16b5deedaee99e2546c42244d3dbeef88cf67403
Omar Flores <omar.garciaf247@gmail.com>
2019-02-17 13:07:17 -0500

    UI update

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,13 +1,14 @@
 $(document).ready(() => {
-    let link;
-    $("#submit").click(() => {
-      link = $("#user").val();
-  
-      console.log("requesting: ", link);
-  
-      $.get("/music", (data) => {
+    $.get("/music", (data) => {
         console.log("Result playlist");
+        let temp = document.getElementById('allTracks').innerHTML;
+
+        for (let i = 0; i < data[0].length; i++) {
+            temp += `<iframe src="https://open.spotify.com/embed/track/${data[0][i].track.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+        }
+
+        document.getElementById('allTracks').innerHTML = temp;
+
         console.log(data);
-      });
     });
-  });
+});
